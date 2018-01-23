373fe4d5eb57885c93ce06102c09fb703b50de70
David Öhman <dohman_92@hotmail.com>
2018-01-23 15:10:56 +0100

    Lyckats få M och L knapp funka med 15 sec timer

--- app/src/main/java/com/dohman/boilaneggbae/MainActivity.java
+++ app/src/main/java/com/dohman/boilaneggbae/MainActivity.java
@@ -1,13 +1,67 @@
 package com.dohman.boilaneggbae;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView time;
+    private Button start;
+    private Button cancel;
+    private CountDownTimer countDownTimer;
+
+    private View.OnClickListener btnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch(view.getId()) {
+                case R.id.buttonl :
+                    start();
+                    break;
+                case R.id.buttonm :
+                    cancel();
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.buttonl);
+        start.setOnClickListener(btnClickListener);
+        cancel = findViewById(R.id.buttonm);
+        cancel.setOnClickListener(btnClickListener);
+        time = findViewById(R.id.time);
+    }
+
+    private void start() {
+        time.setText("15");
+
+        countDownTimer = new CountDownTimer(15 * 1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                time.setText("" + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                time.setText("Done!");
+            }
+        };
+
+       countDownTimer.start();
+    }
+
+    private void cancel() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+            countDownTimer = null;
+        }
     }
 }
