c1fe7433d84600d7cb7113df605bc61529d1827f
David Öhman <dohman_92@hotmail.com>
2018-01-25 14:17:15 +0100

    Almost working with M/L

--- app/src/main/java/com/dohman/boilaneggbae/MainActivity.java
+++ app/src/main/java/com/dohman/boilaneggbae/MainActivity.java
@@ -16,14 +16,35 @@ public class MainActivity extends AppCompatActivity {
     private Button buttonMedium;
     private Button buttonHard;
     private CountDownTimer countDownTimer;
+    private EggSize mediumOrLarge = EggSize.UNDEFINED;
     private boolean alreadyRunning = false;
+
+    enum EggSize {
+        UNDEFINED, MEDIUM, LARGE;
+    }
+
+    private View.OnClickListener btnMediumSizeClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mediumOrLarge = EggSize.MEDIUM;
+        }
+    };
+
+    private View.OnClickListener btnLargeSizeClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mediumOrLarge = EggSize.LARGE;
+        }
+    };
 
     private View.OnClickListener btnSoftClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if (alreadyRunning == false) {
+            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
                 alreadyRunning = true;
                 start(220);
+            } else if (mediumOrLarge == EggSize.UNDEFINED) {
+                time.setText("Choose size first");
             } else {
                 alreadyRunning = false;
                 cancel();
@@ -34,9 +55,11 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnMediumClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if (alreadyRunning == false) {
+            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
                 alreadyRunning = true;
                 start(420);
+            } else if (mediumOrLarge == EggSize.UNDEFINED) {
+                time.setText("Choose size first");
             } else {
                 alreadyRunning = false;
                 cancel();
@@ -47,9 +70,11 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnHardClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            if (alreadyRunning == false) {
+            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
                 alreadyRunning = true;
                 start(660);
+            } else if (mediumOrLarge == EggSize.UNDEFINED) {
+                time.setText("Choose size first");
             } else {
                 alreadyRunning = false;
                 cancel();
@@ -63,11 +88,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Hittar knapparna
+        buttonMediumSize = findViewById(R.id.buttonMediumSize);
+        buttonLargeSize = findViewById(R.id.buttonLargeSize);
+
         buttonSoft = findViewById(R.id.buttonSoft);
         buttonMedium = findViewById(R.id.buttonMedium);
         buttonHard = findViewById(R.id.buttonHard);
 
         // Sätter på listener på knapparna
+        buttonMediumSize.setOnClickListener(btnMediumSizeClickListener);
+        buttonLargeSize.setOnClickListener(btnLargeSizeClickListener);
+
         buttonSoft.setOnClickListener(btnSoftClickListener);
         buttonMedium.setOnClickListener(btnMediumClickListener);
         buttonHard.setOnClickListener(btnHardClickListener);
@@ -90,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-       countDownTimer.start();
+        countDownTimer.start();
     }
 
     private void cancel() {
