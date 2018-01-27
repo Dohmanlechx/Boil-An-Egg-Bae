cc72f22bc92464fe5afa857b757a42eb6108b3e3
David Öhman <dohman_92@hotmail.com>
2018-01-27 23:17:18 +0100

    Kinda fixed onSave & onRestore

--- app/src/main/java/com/dohman/boilaneggbae/MainActivity.java
+++ app/src/main/java/com/dohman/boilaneggbae/MainActivity.java
@@ -7,100 +7,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.Locale;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView time;
+    private static final long CURRENT_TIME = 600000;
+    private long mTimeLeftInMillis = CURRENT_TIME;
+    private long mEndTime;
+    private TextView mTextViewCountDown;
+    private boolean mTimerRunning;
     private Button buttonLargeSize;
     private Button buttonMediumSize;
     private Button buttonSoft;
     private Button buttonMedium;
     private Button buttonHard;
     private Button buttonHellaHard;
-    private CountDownTimer countDownTimer;
+    private CountDownTimer mCountDownTimer;
     private EggSize mediumOrLarge = EggSize.UNDEFINED;
     private boolean alreadyRunning = false;
 
     enum EggSize {
-        UNDEFINED, MEDIUM, LARGE;
+        UNDEFINED, MEDIUM, LARGE
     }
-
-    private View.OnClickListener btnMediumSizeClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            mediumOrLarge = EggSize.MEDIUM;
-        }
-    };
-
-    private View.OnClickListener btnLargeSizeClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            mediumOrLarge = EggSize.LARGE;
-        }
-    };
-
-    private View.OnClickListener btnSoftClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
-                alreadyRunning = true;
-                start(240);
-            } else if (mediumOrLarge == EggSize.UNDEFINED) {
-                time.setText("Choose size first");
-            } else {
-                alreadyRunning = false;
-                cancel();
-            }
-        }
-    };
-
-    private View.OnClickListener btnMediumClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
-                alreadyRunning = true;
-                start(420);
-            } else if (mediumOrLarge == EggSize.UNDEFINED) {
-                time.setText("Choose size first");
-            } else {
-                alreadyRunning = false;
-                cancel();
-            }
-        }
-    };
-
-    private View.OnClickListener btnHardClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
-                alreadyRunning = true;
-                start(660);
-            } else if (mediumOrLarge == EggSize.UNDEFINED) {
-                time.setText("Choose size first");
-            } else {
-                alreadyRunning = false;
-                cancel();
-            }
-        }
-    };
-
-    private View.OnClickListener btnHellaHardClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
-                alreadyRunning = true;
-                start(1800);
-            } else if (mediumOrLarge == EggSize.UNDEFINED) {
-                time.setText("Choose size first");
-            } else {
-                alreadyRunning = false;
-                cancel();
-            }
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -126,44 +54,163 @@ public class MainActivity extends AppCompatActivity {
         buttonHellaHard.setOnClickListener(btnHellaHardClickListener);
 
         //Färglägger knapparna
-        buttonSoft.getBackground().setColorFilter(0x00000000 , PorterDuff.Mode.MULTIPLY);
-        buttonMedium.getBackground().setColorFilter(0x00000000 , PorterDuff.Mode.MULTIPLY);
-        buttonHard.getBackground().setColorFilter(0x00000000 , PorterDuff.Mode.MULTIPLY);
-        buttonHellaHard.getBackground().setColorFilter(0x00000000 , PorterDuff.Mode.MULTIPLY);
+        buttonSoft.getBackground().setColorFilter(0x00000000, PorterDuff.Mode.MULTIPLY);
+        buttonMedium.getBackground().setColorFilter(0x00000000, PorterDuff.Mode.MULTIPLY);
+        buttonHard.getBackground().setColorFilter(0x00000000, PorterDuff.Mode.MULTIPLY);
+        buttonHellaHard.getBackground().setColorFilter(0x00000000, PorterDuff.Mode.MULTIPLY);
 
-        time = findViewById(R.id.time);
+        mTextViewCountDown = findViewById(R.id.time);
+
     }
 
-    private void start(int duration) {
-        time.setText("");
+    private View.OnClickListener btnMediumSizeClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mediumOrLarge = EggSize.MEDIUM;
+        }
+    };
+
+    private View.OnClickListener btnLargeSizeClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            mediumOrLarge = EggSize.LARGE;
+        }
+    };
+
+    private View.OnClickListener btnSoftClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
+                alreadyRunning = true;
+                //durationTime = 240;
+                mTimeLeftInMillis = 240000;
+                start();
+            } else if (mediumOrLarge == EggSize.UNDEFINED) {
+                mTextViewCountDown.setText("Choose size first");
+            } else {
+                alreadyRunning = false;
+                cancel();
+            }
+        }
+    };
+
+    private View.OnClickListener btnMediumClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
+                alreadyRunning = true;
+                //durationTime = 420;
+                mTimeLeftInMillis = 420000;
+                start();
+            } else if (mediumOrLarge == EggSize.UNDEFINED) {
+                mTextViewCountDown.setText("Choose size first");
+            } else {
+                alreadyRunning = false;
+                cancel();
+            }
+        }
+    };
+
+    private View.OnClickListener btnHardClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
+                alreadyRunning = true;
+                //durationTime = 660;
+                mTimeLeftInMillis = 660000;
+                start();
+            } else if (mediumOrLarge == EggSize.UNDEFINED) {
+                mTextViewCountDown.setText("Choose size first");
+            } else {
+                alreadyRunning = false;
+                cancel();
+            }
+        }
+    };
+
+    private View.OnClickListener btnHellaHardClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if ((mediumOrLarge != EggSize.UNDEFINED) && (alreadyRunning == false)) {
+                alreadyRunning = true;
+                //durationTime = 1800;
+                mTimeLeftInMillis = 1800000;
+                start();
+            } else if (mediumOrLarge == EggSize.UNDEFINED) {
+                mTextViewCountDown.setText("Choose size first");
+            } else {
+                alreadyRunning = false;
+                cancel();
+            }
+        }
+    };
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putLong("millisLeft", mTimeLeftInMillis);
+        savedInstanceState.putBoolean("timerRunning", mTimerRunning);
+        savedInstanceState.putLong("endTime", mEndTime);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mTimeLeftInMillis = savedInstanceState.getLong("millisLeft");
+        mTimerRunning = savedInstanceState.getBoolean("timerRunning");
+        updateCountDownText();
+
+        if (mTimerRunning) {
+            mEndTime = savedInstanceState.getLong("endTime");
+            mTimeLeftInMillis = mEndTime - System.currentTimeMillis();
+            start();
+        }
+        //durationTime = savedInstanceState.getInt(DURATION_TIME);
+    }
+
+    private void start() {
+        mEndTime = System.currentTimeMillis() + mTimeLeftInMillis;
+        //mTextViewCountDown.setText("");
 
         if (mediumOrLarge == EggSize.MEDIUM) {
-            duration -= 60;
+            mTimeLeftInMillis -= 60000;
         }
-        countDownTimer = new CountDownTimer(duration * 1000, 1000) {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
 
             @Override
             public void onTick(long millisUntilFinished) {
-                String text = String.format(Locale.getDefault(), "%02d:%02d",
-                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) % 60,
-                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60);
-                time.setText(text);
+//                String text = String.format(Locale.getDefault(), "%02d:%02d",
+//                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) % 60,
+//                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60);
+//                mTextViewCountDown.setText(text);
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
             }
 
             @Override
             public void onFinish() {
-                time.setText("The egg is done!");
+                mTimerRunning = false;
             }
-        };
+        }.start();
 
-        countDownTimer.start();
+        mTimerRunning = true;
+        //mCountDownTimer.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        mTextViewCountDown.setText(timeLeftFormatted);
     }
 
     private void cancel() {
-        if (countDownTimer != null) {
-            countDownTimer.cancel();
-            countDownTimer = null;
-            time.setText("Stopped");
+        if (mCountDownTimer != null) {
+            mCountDownTimer.cancel();
+            mCountDownTimer = null;
+            mTextViewCountDown.setText("Stopped");
         }
     }
 }
